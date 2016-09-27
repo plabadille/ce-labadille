a971e766433c361ae9ae2609798a6d83fe3a2810
plabadille <plabadille@gmail.com>
2016-09-27 18:29:55 +0200

    retouche css, refactoring css et dernier ajout d'architecture

--- wp-content/themes/celabTheme/footer.php
+++ wp-content/themes/celabTheme/footer.php
@@ -1,4 +1,7 @@
 <footer id="footer">
+    <div id="socialFooter">
+        <?php echo do_shortcode('[aps-social id="1"]')?>
+    </div>
     <p> Copyright &#169; <?php print(date(Y)); ?> <?php bloginfo('name'); ?> <br /> Blog propulsé par <a href="http://wordpress.org/">WordPress</a> et con&ccedil;u par <a href="http://www.pierrelabadille.com">Pierre Labadille</a> <br />  <?php echo get_num_queries(); ?> requêtes. <?php timer_stop(1); ?> secondes. </p>
 </footer>
 <?php wp_footer(); ?>
