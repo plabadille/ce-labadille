68ef07c6bdabcf3d3225d9d5968e9ed06bf5e2b7
plabadille <plabadille@gmail.com>
2017-07-29 13:21:26 +0200

    update files before domain transfering

--- wp-content/themes/celabTheme/functions.php
+++ wp-content/themes/celabTheme/functions.php
@@ -6,25 +6,15 @@ register_nav_menus(array(
 'mobileMenu' => 'Menu mobile principal (header)'
 ));
 
-//on indique que le theme supporte woo-commerce
 add_action( 'after_setup_theme', 'woocommerce_support' );
 function woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
-//return link of post with slug args
+
 function get_link_by_slug($slug, $type = 'post'){
     $post = get_page_by_path($slug, OBJECT, $type);
     return get_permalink($post->ID);
 }
-
-//custom search excluding pages:
-function SearchFilter($query) {
-    if ($query->is_search) {
-        $query->set('post_type', array('post','product'));
-    }
-    return $query;
-}
-add_filter('pre_get_posts','SearchFilter');
 
 remove_filter('the_content', 'wptexturize');
 
@@ -48,22 +38,3 @@ function clean_posts_column( $columns ) {
     return $columns;
 }
 add_filter( 'manage_edit-post_columns', 'clean_posts_column', 10, 1 );
-
-//fonction donnant les liens de nos catégories perso
-function get_proper_category_url( $categorySlug ) {
-    switch ($categorySlug) {
-        case 'partitionstabs':
-            $categoryLink = "<a href=\"//ce-labadille.com/partitions-tabs\" alt=\"".$categorySlug."\">Partitions Tabs</a>";
-            break;
-        case 'auteurs':
-            $categoryLink = "<a href=\"//ce-labadille.com/artistes\" alt=\"".$categorySlug."\">Artistes</a>";
-            break;
-        case 'chansonshumour':
-            $categoryLink = "<a href=\"//ce-labadille.com/chansons-humour\" alt=\"".$categorySlug."\">Chansons d'humour</a>";
-            break;
-        case '':
-            $categoryLink = "<a href=\"//ce-labadille.com/sample-page\" alt=\"produits\">Produits</a>";
-            break;
-    }
-    return $categoryLink;
-}
--- wp-content/themes/celabTheme/single.php
+++ wp-content/themes/celabTheme/single.php
@@ -2,21 +2,41 @@
 <main id="main">
     <section id="content">
         <?php
-        //loop wordpress
+            $parentUrl = null;
+            $parentTitle = null;
+            $exist = false;
+            switch ($_GET["categorie"]) {
+                case "artistes":
+                    $exist = true;
+                    $parentUrl = get_permalink( get_page_by_path( $_GET["categorie"] ) );
+                    $parentTitle = get_the_title( get_page_by_path( $_GET["categorie"] ) );
+                    break;
+                case "chansons-humour":
+                    $exist = true;
+                    $parentUrl = get_permalink( get_post( '23' ) ); //on utilise l'id car le slug ne fonctionne pas pour un obscure raison pour cette page.
+                    $parentTitle = get_the_title( get_post( '23' ) );
+                    break;
+                case "partitionstabs":
+                    $exist = true;
+                    $parentUrl = get_permalink( get_post( '21' ) ); //on utilise l'id car le slug ne fonctionne pas pour un obscure raison pour cette page.
+                    $parentTitle = get_the_title( get_post( '21' ) );
+                    break;
+                default:
+                    echo "Le fil d'ariane n'est pas encore paramétré pour cette catégorie, merci de prévenir un administrateur.";
+                    break;
+            }
+
+        if ($exist){
+            ?>
+            <aside id="ariane">
+                <p><a href="<?php echo get_option('home'); ?>" alt="accueil">Home</a> » <a href="<?php echo $parentUrl ?>" alt="<?php echo $parentTitle ?>"><?php echo $parentTitle ?></a> » <?php wp_title(); ?> </p>
+            </aside>
+        <?php } ?>
+        <?php //loop wordpress
         if (have_posts()) {
             while (have_posts()){
                 the_post();
-                //ariane
-                $categorySlug = get_the_category()[0]->slug;
-                $categoryLink = get_proper_category_url($categorySlug);
-                if ($categoryLink){
-                    ?>
-                    <aside id="ariane">
-                        <p><a href="<?php echo get_option('home'); ?>" alt="accueil">Home</a> » <?php echo $categoryLink; ?>  » <?php wp_title(); ?> </p>
-                    </aside>
-                <?php } ?>
-
-                <article class="post" id="post-<?php the_ID(); ?>">
+                ?><article class="post" id="post-<?php the_ID(); ?>">
                 <div class="post_content">
                     <?php the_content(); ?>
                 </div>
